8db7a10f3375a2654e44e482d1066ecb73ffa6f3
user9x9 <4nil000@protonmail.com>
2025-10-20 13:33:05 +0000

    edit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,6 @@
 int main(void) /* Creer fonction main qui est vide */
 {
 puts("\"Programming is like building a multilingual puzzle"); 
-
 /* Affiche ce texte avec un saut a la ligne */
-
 return (0); /* Retourne 0 */
 }

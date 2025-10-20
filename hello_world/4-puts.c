24a868490f875c9a7f1157e67eacb41259e82ec9
user9x9 <4nil000@protonmail.com>
2025-10-20 13:30:35 +0000

    edit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void) /* Cr√er fonction main qui est vide */
+int main(void) /* Creer fonction main qui est vide */
 {
- puts("\"Programming is like building a multilingual puzzle"); /* Affiche ce texte avec un saut a la ligne */
- return (0); /* Retourne 0 */
+puts("\"Programming is like building a multilingual puzzle"); /* Affiche ce texte avec un saut a la ligne */
+return (0); /* Retourne 0 */
 }

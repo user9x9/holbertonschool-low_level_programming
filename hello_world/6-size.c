0182204300c82bd42c809da34a12e13e094603c8
user9x9 <4nil000@protonmail.com>
2025-10-21 08:27:58 +0000

    Add file

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Creation de la fonction main
+ * Description: Affiche les differentes tailles de plusieurs types de variables
+ * Return: 0
+ */
+int main(void)
+{
+    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+    printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(double));
+    return (0);
+}

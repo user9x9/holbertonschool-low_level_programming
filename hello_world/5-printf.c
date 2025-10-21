0a4571b3e0b9bef58da66131c43e98f5faf6505f
user9x9 <4nil000@protonmail.com>
2025-10-21 08:03:52 +0000

    Add file

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - creation de la fonction
+ * Description - afficher du txt avec printf
+ * return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
+}

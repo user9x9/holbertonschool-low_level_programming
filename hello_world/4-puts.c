a42f4597d1705a0aa65fd6f7efe910f878be488d
user9x9 <4nil000@protonmail.com>
2025-10-21 07:42:45 +0000

    edit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /* Creer une fonction main vide
  * Affiche le texte souhaite
- * Retourne 0 
+ * Retourne 0
  * */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+return (0);
 }

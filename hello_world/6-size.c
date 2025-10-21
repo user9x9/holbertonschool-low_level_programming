9147aa6da796fbb9ac1ecea3cf2d6afd5a530e8a
user9x9 <4nil000@protonmail.com>
2025-10-21 08:33:40 +0000

    edit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,6 +11,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
-printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(double));
 return (0);
 }

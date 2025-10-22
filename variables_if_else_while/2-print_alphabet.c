b33fc28c587b8deac26d26c2051b13524cd59c57
user9x9 <4nil000@protonmail.com>
2025-10-22 07:45:24 +0000

    edit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Script qui affiche l alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	
+	putchar('\n');
+	return (0);
+}

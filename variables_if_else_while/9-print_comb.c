6d1d34fef2ef74c673e2608af2838dd19b74cde0
user9x9 <4nil000@protonmail.com>
2025-10-22 12:27:42 +0000

    Add file

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible
+ * combinations of single-digit numbers.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+    int x = 48;
+
+    while (x < 58)
+    {
+        putchar(x);
+        if (x < 57)
+        {
+            putchar(44);
+            putchar(32);
+        }
+        x++;
+    }
+    putchar('\n');
+    return (0);
+}
+

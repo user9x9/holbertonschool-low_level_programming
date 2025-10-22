be86741b63b564540d351179dfe71926098696ba
user9x9 <4nil000@protonmail.com>
2025-10-22 07:56:06 +0000

    edit

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/** main - Script qui ecrit alphabet en min et maj
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+	char alphabetMaj = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	while (alphabetMaj <= 'Z') 
+	{
+		putchar(alphabetMaj);
+                alphabetMaj++;
+	}
+
+	putchar('\n');
+	return (0);
+}

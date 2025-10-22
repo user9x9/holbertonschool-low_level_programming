5c631bbdfde9a222e8021698a6514e2d9412fcf5
user9x9 <4nil000@protonmail.com>
2025-10-22 12:30:53 +0000

    edit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,19 +9,19 @@
 
 int main(void)
 {
-    int x = 48;
+	int x = 48;
 
-    while (x < 58)
-    {
-        putchar(x);
-        if (x < 57)
-        {
-            putchar(44);
-            putchar(32);
-        }
-        x++;
-    }
-    putchar('\n');
-    return (0);
+	while (x < 58)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
 }
 

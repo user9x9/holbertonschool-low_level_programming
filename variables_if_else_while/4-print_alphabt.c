82f7871386da6fa28458a5a26cf752edda4d7f81
user9x9 <4nil000@protonmail.com>
2025-10-22 08:10:02 +0000

    Add file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche alphabet sans q et e
+ *
+ * Return: 0
+ */
+ 
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet)
+			alphabet++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

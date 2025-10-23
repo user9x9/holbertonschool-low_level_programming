a84def0d652c4fd063907eeeaf191058afbfc9a9
user9x9 <4nil000@protonmail.com>
2025-10-23 12:45:17 +0000

    Add file

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Affiche l alphabet en min
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet = 'a';
+
+	int compteur;
+
+	while (compteur <= 10)
+	{
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+
+	}
+	_putchar('\n');
+	compteur++;
+}

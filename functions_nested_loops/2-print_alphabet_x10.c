30468f0a1fba427697f3b1a3727b5941eb73b97d
user9x9 <4nil000@protonmail.com>
2025-10-23 13:42:54 +0000

    edit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet - Affiche l alphabet en min
+ * print_alphabet_x10 - Affiche l alphabet 10 fois en min
  *
  * Return: 0
  */
 
 void print_alphabet_x10(void)
 {
-	char alphabet = 'a';
 
 	int compteur = 0;
 
 	while (compteur <= 10)
 	{
+		char alphabet = 'a';
+
 		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);

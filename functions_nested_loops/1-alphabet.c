ceaac0526e2ba399701090be0fb43b77ea7ba256
user9x9 <4nil000@protonmail.com>
2025-10-23 12:20:31 +0000

    edit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,16 +7,14 @@
  */
 
 void print_alphabet(void);
-
-void print_alphabet(void);
 {
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
-		putchar(alphabet);
+		_putchar(alphabet);
 		alphabet++;
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }

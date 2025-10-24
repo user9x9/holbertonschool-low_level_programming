7caf7be9c9ccde4cf075ed2d69a83728f3477c9f
user9x9 <4nil000@protonmail.com>
2025-10-24 12:16:13 +0000

    edit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * jack_baueur = Affiche de 00:00 a 23:59
+ *
+ * Return: Rien
+ *
+ */
+void jack_bauer(void)
+{
+	int heure = 0;
+	int minute = 0;
+
+	while (heure < 24)
+	{
+		while (minute < 60)
+		{
+			_putchar((heure / 10) + '0');
+			_putchar((heure % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+			minute++;
+		}
+		heure++;
+	}
+
+
+}

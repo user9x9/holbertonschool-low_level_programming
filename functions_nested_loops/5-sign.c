920a529cda76f1e73b2eb5202f588810388d3f86
user9x9 <4nil000@protonmail.com>
2025-10-24 08:43:08 +0000

    edit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printtf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+}

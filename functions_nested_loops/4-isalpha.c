ba4107f5a1f7f4cad81d745dbfb2de10574545e6
user9x9 <4nil000@protonmail.com>
2025-10-24 08:27:16 +0000

    edit

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,9 +2,15 @@
 #include <ctype.h>
 #include "main.h"
 
+/**
+ * @c: Le caractere a verifier
+ *
+ * Return: 1 si cest une lettre min ou maj sinn 0
+ */
+
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}

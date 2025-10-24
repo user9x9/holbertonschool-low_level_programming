d28aa8597f11dde32273fa2c6e5c4176046a7acb
user9x9 <4nil000@protonmail.com>
2025-10-24 09:01:00 +0000

    edit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Calcule la valeur absolue dun entier etrenvoi en positif
+ *
+ * @n: Lentier a check
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,4 +11,6 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int);
+
 #endif /* MAIN_H */

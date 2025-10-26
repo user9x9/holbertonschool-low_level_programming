ed16bc304ffbfa77b448e26dcd32a884e3a3a490
user9x9 <4nil000@protonmail.com>
2025-10-26 19:50:57 +0000

    edit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include "main.h"
 
+
+/**
+ * print_to_98 - Affiche tous les nombres de n a 98
+ *
+ * @n: L entier de base
+ *
+ */
+
 void print_to_98(int n)
 {
-
-	
 	if (n < 98)
 	{
 		printf("%d", n);
@@ -17,14 +23,12 @@ void print_to_98(int n)
 			n++;
 		}
 	}
-		
 
 
 		else
 		{
 			printf("%d", n);
 			n--;
-			
 			while (n >= 98)
 			{
 				printf(", %d", n);

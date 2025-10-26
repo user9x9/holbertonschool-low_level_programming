271836764cc5282f40e86fe53f7c12ddaf35f107
user9x9 <4nil000@protonmail.com>
2025-10-26 18:16:38 +0000

    Add file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - Ajoute deux entiers et retourne le resultat
+ *
+ * @a: Premier entier
+ * 
+ * @b: Deuxieme entier
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a +b);
+}

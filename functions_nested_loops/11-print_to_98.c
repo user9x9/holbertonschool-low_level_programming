045a36282d097f2663c75dd67ea8ec7157cb922d
user9x9 <4nil000@protonmail.com>
2025-10-26 18:55:46 +0000

    edit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,12 +4,32 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
-	{
-		_putchar(',');
-		_putchar(' ');
-		n++;
+	if (n <= 98)
+	{	
+		while (n <= 98)
+		{
+			printf("%d", n);
+			n++;
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
 
-	return (0);
+
+		else
+		{
+			while (n >= 98)
+			{
+				printf("%d", n);
+				n--;
+			}
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
 }

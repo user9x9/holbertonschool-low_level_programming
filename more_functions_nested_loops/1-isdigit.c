726887551175eaff0622d6538fe94ac1db81410b
user9x9 <4nil000@protonmail.com>
2025-10-27 09:26:37 +0000

    edit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -13,12 +13,9 @@ int print_last_digit(int n)
 {
 	int lastDigit = n % 10;
 
-	if (lastDigit < 0)
+	if (lastDigit >= 0 && lastDigit <= 9)
 	{
-		lastDigit = -lastDigit;
+		return (1);
 	}
-
-	_putchar(lastDigit + '0');
-
-	return (lastDigit);
+	return (0);
 }

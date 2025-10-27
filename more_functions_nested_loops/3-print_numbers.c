d193e85c475f60c248804724b3ba76b5f556c735
user9x9 <4nil000@protonmail.com>
2025-10-27 10:03:31 +0000

    edit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_numbers - Affiche les nombres de 0 jusqua 9
+ *
+ * @a: La variable qui contient le premier nombre
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a != 9)
+	{
+		_putchar(n + '0')
+		a++;
+		return;
+	}
+}

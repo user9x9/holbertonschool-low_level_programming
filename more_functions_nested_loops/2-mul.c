9ee7da66731ebe60fa6b5de5a22b4ffa9a06a489
user9x9 <4nil000@protonmail.com>
2025-10-27 09:57:03 +0000

    edit

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * mul - Fonction qui multiplie deux entiers
+ *
+ * @a: premier entier
+ *
+ * @b: deuxieme entier
+ *
+ * Return: a*b
+ */
+
+
 int mul(int a, int b)
 {
-	return (a + b);
+	return (a * b);
 }

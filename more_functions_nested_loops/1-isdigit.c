8dcfdf85e9e261bc6365483950a3b0c943ff5c8c
user9x9 <4nil000@protonmail.com>
2025-10-27 09:27:59 +0000

    edit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * print_last_digit - Prints the last digit of a number
+ * _isdigit - Prints the last digit of a number
  *
  * @n: The number to check
  *
  * Return: the value of the last digit
  *
  */
-int print_last_digit(int n)
+int _isdigit(int n)
 {
 	int lastDigit = n % 10;
 

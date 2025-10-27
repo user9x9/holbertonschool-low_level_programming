4b971cdf16fa68b44a0bb8394b6bbcaf2f8dd6bd
user9x9 <4nil000@protonmail.com>
2025-10-27 10:21:14 +0000

    edit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9 without 2 and 4
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a == 1 || a == 3)
+		{
+			a = a + 2;
+		}
+		_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n');
+}

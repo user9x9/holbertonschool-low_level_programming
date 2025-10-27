445e68940434ce71e78c10a46f991ed93dea62c6
user9x9 <4nil000@protonmail.com>
2025-10-27 10:44:10 +0000

    edit

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void more_numbers(void)
+{
+	int a = 0;
+	int b = 0;
+
+
+	while (a <= 14)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+
+	while (b <= 10)
+	{
+		_putchar(a + '0');
+		b++
+	}
+	_putchar('\n');
+}

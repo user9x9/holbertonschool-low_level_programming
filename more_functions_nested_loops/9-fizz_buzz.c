0ccf121333bff864a6e504edb0a2c0b5ef494d39
user9x9 <4nil000@protonmail.com>
2025-10-28 10:05:40 +0000

    edit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int a = 1;
+
+	while (a <= 100)
+	{
+
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		printf(" ");
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}

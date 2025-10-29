000db24d9b57a6bfd2255e373b77418ec2826d06
user9x9 <4nil000@protonmail.com>
2025-10-29 18:54:03 +0000

    edit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Affiche la chaine de caractere en reverse
+ *
+ * @s: Pointeur dla chaine de caractere
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	int b = a - 1;
+
+	while (b >= 0)
+	{
+		_putchar(s[b]);
+		b--;
+	}
+	_putchar('\n');
+}

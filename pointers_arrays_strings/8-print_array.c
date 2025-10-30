af0d7214ced792f6e0ee0b35929d20536adbd365
user9x9 <4nil000@protonmail.com>
2025-10-30 11:36:58 +0000

    edit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_array - Affiche les valeurs du tableau
+ *
+ * @a: Le tableau
+ * @n: Les elements du tableau
+ *
+ * Return: void
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}

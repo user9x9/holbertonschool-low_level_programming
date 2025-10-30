7d878f4d0e3461b1e47bf95b8fbcba6b908014f6
user9x9 <4nil000@protonmail.com>
2025-10-30 11:14:28 +0000

    edit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - Affiche a partir de la moitie dla chaine de caracteres
+ *
+ * @str: la chaine de caracteres
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int debut = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+
+	}
+
+	if (a % 2 == 0)
+	{
+		debut = a / 2;
+	}
+	else 
+	{
+		debut = (a + 1) / 2; 
+	}
+
+	while (str[debut] != '\0')
+	{
+		_putchar(str[debut]);
+		debut++;
+	}
+	_putchar('\n');
+}

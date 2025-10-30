fac87117e63d6fad69b55ad6ecb56ee277ebf44a
user9x9 <4nil000@protonmail.com>
2025-10-30 09:18:28 +0000

    edit

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Affiche un caractere sur deux 
+ *
+ * @str: La chaine de caracteres
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i+=2;
+	}
+	_putchar('\n');
+}

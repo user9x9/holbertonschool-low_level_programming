221d88fbccbde5de05f07d0d06422d9720a09ded
user9x9 <4nil000@protonmail.com>
2025-10-31 13:57:41 +0000

    edit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatene deux chaines
+ *
+ * @dest: the destination string where src will be appended
+ * @src: the source string that will be appended to dest
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, const char *src, int n) {
+    int i = 0;
+    int a = 0;
+
+    while (dest[i] != '\0') {
+        i++;
+    }
+
+    while (n > 0 && src[a] != '\0') {
+        dest[i] = src[a];
+        i++;
+        a++;
+        n--;
+    }
+
+    dest[i] = '\0';
+
+    return (dest);
+}

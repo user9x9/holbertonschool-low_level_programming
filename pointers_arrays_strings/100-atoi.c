8a302e65725fa329d8e9d7db7a9a40bb19f203af
user9x9 <4nil000@protonmail.com>
2025-10-31 09:55:28 +0000

    edit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,27 +5,38 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
+	int i = 0;
 
 	while (*s == ' ')
 	{
 		s++;
 	}
 
-	if (*s == '-')
+	while ((*s == '+' || *s == '-') && i == 0)
 	{
-		sign = -sign;
-		s++;
+		if (*s == '-')
+		{
+			sign = -sign;
+		}
+	s++;
 	}
 
-	else if (*s == '+')
+	while (*s != '\0')
 	{
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-		result = result * 10 + (*s - '0');
-		s++;
+		if (*s >= '0' && *s <= '9')
+		{
+			i = 1;
+			result = result * 10 + (*s - '0');
+			s++;
+		}
+		else if (i)
+		{
+			break;
+		}
+		else
+		{
+			s++;
+		}
 	}
 	return result * sign;
 }

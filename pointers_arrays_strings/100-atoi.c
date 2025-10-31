e6b2af4e8d59d8ba467c3524fdea2d6ac1aae8f7
user9x9 <4nil000@protonmail.com>
2025-10-31 10:17:47 +0000

    edit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,43 @@
-#include <stdio.h>
 #include "main.h"
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+    int result = 0;
+    int sign   = 1;
+    int i = 0;
 
-	while (*s == ' ')
-	{
-		s++;
-	}
+    while (*s == ' ')
+        s++;
 
-	while (*s && *s != '+' && *s != '-'&& !(*s >= '0' && *s <= '9'))
-	{
-		s++;
-	}
-	
-	while (*s == '+' || *s == '-')
-	{
-		if (*s == '-')
-		{
-			sign = -sign;
-			s++;
-		}
-	}
+    while (*s == '+' || *s == '-')
+    {
+        char *p = s;
+        while (*p == '+' || *p == '-') p++;
+        if (*p < '0' || *p > '9')
+            break;
 
-	while (*s != '\0')
-	{
-		if (*s >= '0' && *s <= '9')
-		{
-			i = 1;
-			result = result * 10 + (*s - '0');
-			s++;
-		}
-		else if (i)
-		{
-			break;
-		}
-		else
-		{
-			s++;
-		}
-	}
-	return result * sign;
+        if (*s == '-')
+            sign = -sign;
+        s++;
+    }
+
+    while (*s != '\0')
+    {
+        if (*s >= '0' && *s <= '9')
+        {
+            i = 1;
+            result = result * 10 + (*s - '0');
+            s++;
+        }
+        else if (i)
+        {
+            break;
+        }
+        else
+        {
+            s++;
+        }
+    }
+
+    return result * sign;
 }

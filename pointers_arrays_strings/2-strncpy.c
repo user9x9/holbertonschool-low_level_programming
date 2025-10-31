6d70aa025a9a9e56d9e52363e403909cffdc57dd
user9x9 <4nil000@protonmail.com>
2025-10-31 14:17:59 +0000

    edit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
-* _strcpy - copies a string
-*
-*
-*
-*
-*/
+ * _strncpy - copie une chaîne de caractères
+ * @dest: destination où la chaîne sera copiée
+ * @src: chaîne source à copier
+ * @n: nombre maximal de caractères à copier
+ *
+ * Return: un pointeur vers la chaîne de destination (dest)
+ */
 
 char *_strcpy(char *dest, const char *src, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    for ( ; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
-    return (dest);
+	return (dest);
 }

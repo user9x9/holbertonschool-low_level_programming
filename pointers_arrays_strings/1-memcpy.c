08075db4ccb3154711d7e33d3d03e6f4cf448087
user9x9 <4nil000@protonmail.com>
2025-11-03 09:43:43 +0000

    edit

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memcpy - copie n octets de la mémoire src vers dest
+* @dest: buffer de destination
+* @src: buffer source
+* @n: nombre d'octets à copier
+*
+* Retour: un pointeur vers dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -33,4 +33,6 @@ char *_strncpy(char *dest, char *src, int n);
 
 char *_memset(char *s, char b, unsigned int n);
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif /* MAIN_H */

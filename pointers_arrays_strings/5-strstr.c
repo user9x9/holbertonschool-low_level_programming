7fdb6fb835bf675fccc81634513059c2cde71292
user9x9 <4nil000@protonmail.com>
2025-11-03 13:27:26 +0000

    edit

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+* _strstr - localise la première occurrence de la sous-chaîne
+* needle dans la chaîne haystack.
+* @haystack: la chaîne dans laquelle chercher
+* @needle: la sous-chaîne à trouver
+*
+* Return: un pointeur vers le début de la sous-chaîne needle dans haystack,
+*         ou NULL si la sous-chaîne n'est pas trouvée.
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
@@ -10,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		return (haystack);
 	}
-	
+
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[a])

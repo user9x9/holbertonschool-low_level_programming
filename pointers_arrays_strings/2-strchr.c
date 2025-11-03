c61319c992e9548f39d98659b680a1f3af500ed4
user9x9 <4nil000@protonmail.com>
2025-11-03 10:14:12 +0000

    edit

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,16 +14,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (1)
 	{
 		if (s[i] == c)
-		{
 			return (&s[i]);
-		}
-		else if (s[i] == '\0')
-		{
+		if (s[i] == '\0')
 			break;
-		}
 		i++;
 	}
 	return (NULL);

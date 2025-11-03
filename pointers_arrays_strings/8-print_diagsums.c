01f7914059623daa93c26beb91ac60170a0829c1
user9x9 <4nil000@protonmail.com>
2025-11-03 14:16:49 +0000

    edit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,16 @@
 
 void print_diagsums(int *a, int size)
 {
-    int i = 0;
-    int n1 = 0;
-    int n2 = 0;
+	int i = 0;
+	int n1 = 0;
+	int n2 = 0;
 
-    while (i < size)
-    {
-        n1 = n1 + a[i * size + i];
-        n2 = n2 + a[i * size + (size - i - 1)];
+	while (i < size)
+	{
+		n1 = n1 + a[i * size + i];
+		n2 = n2 + a[i * size + (size - i - 1)];
 
-        i++;
-    }
-    printf("%d, %d\n", n1, n2);
+		i++;
+	}
+	printf("%d, %d\n", n1, n2);
 }

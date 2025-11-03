d98f1e6d7c069ee33439d75e38dfda562b7406ee
user9x9 <4nil000@protonmail.com>
2025-11-03 09:59:45 +0000

    edit

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _strchr - localise la première occurrence d'un caractère dans une chaîne
- * @s: la chaîne dans laquelle chercher
- * @c: le caractère à trouver
- *
- * Retourne un pointeur vers le premier caractère c trouvé dans s,
- * ou NULL si le caractère n'est pas présent.
- */
+* _strchr - localise la première occurrence d'un caractère dans une chaîne
+* @s: la chaîne dans laquelle chercher
+* @c: le caractère à trouver
+*
+* Retourne un pointeur vers le premier caractère c trouvé dans s,
+* ou NULL si le caractère n'est pas présent.
+*/
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)

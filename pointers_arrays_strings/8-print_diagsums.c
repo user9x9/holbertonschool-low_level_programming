902dc04e64061cd16432d9b48382c5636c60419c
user9x9 <4nil000@protonmail.com>
2025-11-03 14:16:24 +0000

    edit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* print_diagsums - Affiche la somme des deux diagonales d'une matrice carrée.
+* @a: Pointeur vers la matrice (traitée comme un tableau unidimensionnel)
+* @size: Taille de la matrice (dimension de la matrice carrée)
+*/
+
 void print_diagsums(int *a, int size)
 {
     int i = 0;
@@ -11,7 +17,7 @@ void print_diagsums(int *a, int size)
     {
         n1 = n1 + a[i * size + i];
         n2 = n2 + a[i * size + (size - i - 1)];
-        
+
         i++;
     }
     printf("%d, %d\n", n1, n2);

3282c454fc5ff4cdd297317ed902e31658c078da
user9x9 <4nil000@protonmail.com>
2025-11-03 12:50:32 +0000

    edit

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,38 @@
 #include "main.h"
 
+/**
+* _strspn - obtient la longueur d'un préfixe de sous-chaîne
+* @s: la chaîne à rechercher
+* @accept: les caractères à comparer dans la chaîne s
+*
+* Return: le nombre d'octets dans le segment initial de s
+*         qui ne contiennent que des caractères présents dans accept.
+*/
+
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i = 0;
-    unsigned int a = 0;
-    unsigned int count = 0;
+	unsigned int i = 0;
+	unsigned int a = 0;
+	unsigned int count = 0;
 
-    while (s[i] != '\0')
-    {
-        a = 0;
+	while (s[i] != '\0')
+	{
+		a = 0;
 
-        while (accept[a] != '\0')
-        {
-            if (s[i] == accept[a])
-            {
-                break;
-            }
-        a++;
-        }
-            if (accept[a] == '\0')
-            {
-                return (count);
-            }
-    count++;
-    i++;
-    }
-    return (count);
+		while (accept[a] != '\0')
+		{
+			if (s[i] == accept[a])
+			{
+				break;
+			}
+		a++;
+		}
+			if (accept[a] == '\0')
+			{
+				return (count);
+			}
+	count++;
+	i++;
+	}
+	return (count);
 }

64314d989488cd61aabb9d2476a3cbc9010ebb65
user9x9 <4nil000@protonmail.com>
2025-11-05 09:22:16 +0000

    edit

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
+/**
+* factorial - Returns the factorial of n
+* @n: The number
+* Return: Factorial of n or -1 is its lower than O
+*/
+
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
 }

f7e67598b97cb992e05a9fb824a51d0c1c136ce0
user9x9 <4nil000@protonmail.com>
2025-11-05 09:07:44 +0000

    edit

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+* _strlen_recursion - Prints the length of a string 
+* @s: The string
+* Return: length of s
+*/
+
 int _strlen_recursion(char *s)
 {
 	if (s[0] == '\0' )

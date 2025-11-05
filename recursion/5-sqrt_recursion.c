b6e49c55bb3cd91646876c768823e9e2130661bf
user9x9 <4nil000@protonmail.com>
2025-11-05 09:41:54 +0000

    edit

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+    int i = 1;
+
+    if (n < 0)
+    {
+        return (-1);
+    }
+    else if (n == 0)
+    {
+        return (1);
+    }
+    else
+    {
+        return (sqrt_helper(n, 1));
+    }
+}
+
+int sqrt_helper(int n, int i)
+{
+    if (i * i == n)
+    {
+        return (i);
+    }
+    else if (i * i > n)
+    {
+        return (-1);
+    }
+    else
+    {
+        return (sqrt_helper(n, i + 1));
+    }
+}

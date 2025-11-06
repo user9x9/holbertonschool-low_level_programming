e70842dc12cc249e8e3942828720688005113319
user9x9 <4nil000@protonmail.com>
2025-11-06 13:24:44 +0000

    edit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char argv[])
+/**
+* main - Prints the program name
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
 {
-    printf("%s", argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }

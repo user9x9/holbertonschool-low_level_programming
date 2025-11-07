1c77db9bafaa569688400134a93cbcf089cac70f
user9x9 <4nil000@protonmail.com>
2025-11-07 08:36:02 +0000

    edit

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/** 
+* main - Prints the number of arguments passed into it
+* @argc: The number of arguments passed to the program
+* @argv: Array of string representing the arguments
+* Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }

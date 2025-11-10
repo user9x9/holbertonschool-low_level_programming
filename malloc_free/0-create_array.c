247f7b8b4782556772c030b627056051f17fb028
user9x9 <4nil000@protonmail.com>
2025-11-10 09:26:03 +0000

    Add files

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    if (size == 0)
+    {
+        return (NULL);
+    }
+
+    char *array = malloc(size * sizeof(char));
+
+    if (array == NULL)
+    {
+        exit(1);
+    }
+
+    return()
+
+
+
+
+
+}

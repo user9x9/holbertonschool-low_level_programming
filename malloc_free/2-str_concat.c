35618ef6c99c38cf76145eac0eea79d08da6d4b3
user9x9 <4nil000@protonmail.com>
2025-11-10 14:49:56 +0000

    edit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,50 +1,65 @@
 #include <stdlib.h>
 
+/**
+ * str_concat - Function that concatenates two strings
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ *
+ * Return: A pointer to the newly allocated memory containing the concatenated
+ * string. NULL if memory allocation fails or if both strings are NULL.
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-    unsigned int i, a = 0;
-    char *result;
+	unsigned int i = 0, a = 0;
+	char *result;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-    while (s1[i] != '\0')
-    {
-        i++;
-    }
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
 
-    while (s2[a] != '\0')
-    {
-        a++;
-    }
 
-    result = malloc((i + a + 1) * sizeof(char));
+	while (s2[a] != '\0')
+	{
+		a++;
+	}
 
-    if (result == NULL)
-    {
-        return (NULL);
-    }
 
-    while (s1[i] != '\0')
-    {
-        result[i] = s1[i];
-        i++;
-    }
+	result = malloc((i + a + 1) * sizeof(char));
 
-    while (s2[a] != '\0')
-    {
-        result[i + a] = s2[a];
-        a++;
-    }
+	if (result == NULL)
+	{
+		return (NULL);
+	}
 
-    result[i + a] = '\0';
+	i = 0;
 
-    return (result);
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+
+	a = 0;
+
+	while (s2[a] != '\0')
+	{
+		result[i + a] = s2[a];
+		a++;
+	}
+
+	result[i + a] = '\0';
+
+	return (result);
 }

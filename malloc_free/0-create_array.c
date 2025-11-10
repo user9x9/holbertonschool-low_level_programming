2476f58c0ebb6fafaaebe1e194e06cbbea292e48
user9x9 <4nil000@protonmail.com>
2025-11-10 09:49:16 +0000

    edit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,24 +12,24 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i = 0;
-    char *array = malloc(size * sizeof(char));
+	unsigned int i = 0;
+	char *array = malloc(size * sizeof(char));
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    if (array == NULL)
-    {
-        exit(1);
-    }
+	if (array == NULL)
+	{
+		exit(1);
+	}
 
-    while (i < size)
-    {
-        array[i] = c;
-        i++;
-    }
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
 
-    return (array);
+	return (array);
 }

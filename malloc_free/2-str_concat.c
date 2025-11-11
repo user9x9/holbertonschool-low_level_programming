4e41ec2e3429bf5434d58dbdad4fc508764cf311
user9x9 <4nil000@protonmail.com>
2025-11-11 19:02:38 +0000

    edit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,6 +13,12 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, a = 0;
 	char *result;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i] != '\0')
 		i++;
 

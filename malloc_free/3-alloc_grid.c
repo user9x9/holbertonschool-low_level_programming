4a74a363d5c908ce263ce80f5ba05d131cd1c406
user9x9 <4nil000@protonmail.com>
2025-11-11 11:26:54 +0000

    edit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,37 +10,36 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **a;
-    int i = 0, n = 0;
+	int **a;
+	int i = 0, n = 0;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    a = malloc(sizeof(int *) * height);
+	a = malloc(sizeof(int *) * height);
 
-    while (i < height)
-    {
-        a[i] = malloc(sizeof(int) * width);
+	while (i < height)
+	{
+		a[i] = malloc(sizeof(int) * width);
 
-        if (a[i] == NULL)
-        {
-            while (i > 0)
-            {
-                i--;
-                free(a[i]);
-            }
-                free(a);
-                return(NULL);
-        }
-        n = 0;
+		if (a[i] == NULL)
+		{
+			while (i > 0)
+			{
+				i--;
+				free(a[i]);
+			}
+			free(a);
+			return (NULL);
+		}
+		n = 0;
 
-        while (n < width)
-        {
-            a[i][n] = 0;
-            n++;
-        }
-        i++;
-    }
-    
-    return (a);
+		while (n < width)
+		{
+			a[i][n] = 0;
+			n++;
+		}
+		i++;
+	}
+	return (a);
 }

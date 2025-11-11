56579c49da2cf8bbec6514d733bf51d147fe9821
user9x9 <4nil000@protonmail.com>
2025-11-11 11:25:53 +0000

    edit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,13 @@
 #include <stdlib.h>
 
+/**
+ * alloc_grid - creates a 2D grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer to the 2D grid initialized to 0,
+ * NULL on failure or if width/height <= 0
+ */
+
 int **alloc_grid(int width, int height)
 {
     int **a;
@@ -13,6 +21,17 @@ int **alloc_grid(int width, int height)
     while (i < height)
     {
         a[i] = malloc(sizeof(int) * width);
+
+        if (a[i] == NULL)
+        {
+            while (i > 0)
+            {
+                i--;
+                free(a[i]);
+            }
+                free(a);
+                return(NULL);
+        }
         n = 0;
 
         while (n < width)
@@ -21,16 +40,6 @@ int **alloc_grid(int width, int height)
             n++;
         }
         i++;
-    }
-    if (a[i] == NULL)
-    {
-        while (i > 0)
-        {
-            i--;
-            free(a[i]);
-        }
-        free(a);
-        return(NULL);
     }
     
     return (a);

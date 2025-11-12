5a140ad0c7a558b121771ccdfb212c28af40e581
user9x9 <4nil000@protonmail.com>
2025-11-12 08:45:21 +0000

    edit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,18 +5,18 @@
 /**
 * malloc_checked - Function that allocates memory using malloc
 * @b: Number of bytes to allocate
-* Return: A pointer to the allocated memory 
+* Return: A pointer to the allocated memory
 * otherwise exit the process with status 98 if malloc fails
 */
 
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
-    if (ptr == NULL)
-    {
-        exit (98);
-    }
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
 
-    return (ptr);
+	return (ptr);
 }

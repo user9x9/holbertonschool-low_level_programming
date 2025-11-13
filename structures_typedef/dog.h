5672ddfa6d951031c0637ae5fe4efff4df3cd343
user9x9 <4nil000@protonmail.com>
2025-11-13 10:30:21 +0000

    edit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - Structure décrivant un chien
+* @name: Nom du chien
+* @age: Age du chien
+* @owner: Nom du proprietaire du chien
+* Description: Structure qui contient les informations principales d'un chien
+*/
+
 typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog;
 
 #endif

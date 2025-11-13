f4675c55fe486ff8c6c6ec8f29f41d0f4be38310
user9x9 <4nil000@protonmail.com>
2025-11-13 04:19:04 +0000

    edit

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+	
+	size = max - min + 1
+
+	array = malloc(size * sizeof(int));
+
+	if (array == NULL)
+		return (NULL);
+
+	i = 0;
+
+	while (i < size)
+	{
+		array[i] = min + 1;
+	}
+	return (array);
+}

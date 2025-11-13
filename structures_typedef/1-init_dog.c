6a2f6272d8f12696e724a49d2fc3279bc79411f8
user9x9 <4nil000@protonmail.com>
2025-11-13 14:26:53 +0000

    edit

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* init_dog - Initialize the values in the strut in dog.h
+* @d: Pointer to the structure dog.h
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Name oof the owner of the dog
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,4 +16,6 @@ typedef struct dog
 	char *owner;
 } dog;
 
+void init_dog(dog *d, char *name, float age, char *owner);
+
 #endif

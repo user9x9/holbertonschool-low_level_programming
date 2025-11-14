d40cff155f26aee353ab9b70bb9979412d58a222
user9x9 <4nil000@protonmail.com>
2025-11-14 10:41:33 +0000

    edit

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * str_copy - copie une chaîne de caractères
+ * @src: chaîne à copier
+ *
+ * Return: pointeur vers la nouvelle chaîne, NULL si échec
+ */
+
+char *str_copy(char *src)
+{
+    int i = 0;
+    char *dest;
+
+    if (src == NULL)
+        return (NULL);
+
+    while (src[i] != '\0')
+        i++;
+
+    dest = malloc(i + 1);
+    if (dest == NULL)
+        return (NULL);
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+    
+}
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *d;
+
+    d = malloc(sizeof(dog_t));
+
+    if (d == NULL)
+        return (NULL);
+
+    d->name = str_copy(name);
+    if (d->name == NULL)
+    {
+        free(d);
+        return (NULL);
+    }
+
+    d->owner = str_copy(owner);
+    if (d->owner == NULL)
+    {
+        free(d->name);
+        free(d);
+        return (NULL);
+    }
+    d->age = age;
+    return (d);
+}

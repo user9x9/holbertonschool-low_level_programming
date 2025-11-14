fbf3065929f38d94ee6fb8f900a03eb4e7183555
user9x9 <4nil000@protonmail.com>
2025-11-14 10:44:28 +0000

    edit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,54 +10,53 @@
 
 char *str_copy(char *src)
 {
-    int i = 0;
-    int j = 0;
-    char *dest;
+	int i = 0;
+	int j = 0;
+	char *dest;
 
-    if (src == NULL)
-        return (NULL);
+	if (src == NULL)
+		return (NULL);
 
-    while (src[i] != '\0')
-        i++;
+	while (src[i] != '\0')
+		i++;
 
-    dest = malloc(i + 1);
-    if (dest == NULL)
-        return (NULL);
+	dest = malloc(i + 1);
+	if (dest == NULL)
+		return (NULL);
 
-    j = 0;
-    while (src[j] != '\0')
-    {
-        dest[j] = src[j];
-        j++;
-    }
-    dest[j] = '\0';
-    return (dest);
-    
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
 }
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
+	dog_t *d;
 
-    d = malloc(sizeof(dog_t));
+	d = malloc(sizeof(dog_t));
 
-    if (d == NULL)
-        return (NULL);
+	if (d == NULL)
+		return (NULL);
 
-    d->name = str_copy(name);
-    if (d->name == NULL)
-    {
-        free(d);
-        return (NULL);
-    }
+	d->name = str_copy(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 
-    d->owner = str_copy(owner);
-    if (d->owner == NULL)
-    {
-        free(d->name);
-        free(d);
-        return (NULL);
-    }
-    d->age = age;
-    return (d);
+	d->owner = str_copy(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	d->age = age;
+	return (d);
 }

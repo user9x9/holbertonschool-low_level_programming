89832d7a3af6e599f68a5d5d24ea020f9c5dd644
user9x9 <4nil000@protonmail.com>
2025-11-14 10:50:33 +0000

    edit

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * free_dog - libère la mémoire allouée pour un chien
+ * @d: pointeur vers la structure dog à libérer
+ *
+ * Description: libère les chaines name et owner
+ * puis la structure dog elle meme.
+ */
+
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return (NULL);
+
+    free(d->name);
+    free(d->owner);
+    free(d);
+}

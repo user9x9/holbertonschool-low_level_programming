e220d66207a0f897e31ca13a8c90d55f9776f94c
user9x9 <4nil000@protonmail.com>
2025-11-17 12:55:37 +0000

    edit

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,17 @@
-#include "function_pointers.c"
+#include "function_pointers.h"
 #include <stdlib.h>
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    
+    int i = 0;
+
+    if (array == NULL || cmp == NULL || size <= 0)
+        return (-1);
+
+    while (i < size)
+    {
+        if (cmp(array[i]) != 0)
+            return (i);
+    }
+    return (-1);
 }

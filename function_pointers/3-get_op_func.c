a673b60096db41cb7fd2573bdc9a81f933692482
user9x9 <4nil000@protonmail.com>
2025-11-17 14:04:20 +0000

    edit

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -14,12 +14,13 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
+	int i = 0;
 
-	while (ops->op != NULL)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops->op) == 0)
-			return (ops->f);
-		ops++;
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdio.h>
 
 /**
 * op_add - Returns the sum of a + b
@@ -7,17 +8,37 @@
 * Return: The result of a + b
 */
 
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
 int op_mul(int a, int b)
 {
 	return(a * b);
 }
 
+int op_sub(int a , int b)
+{
+	return (a - b);
+}
+
 int op_div(int a, int b)
 {
-	return(a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return(a % b);
 }

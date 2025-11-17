cdf3345d4888ab56813abf0b47904c146219d111
user9x9 <4nil000@protonmail.com>
2025-11-17 10:24:22 +0000

    edit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,19 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
+/**
+* print_name - Function that prints a name
+* @name: Name
+* @f: Pointer to a function that takes a char* and returns void
+* Description: This function checks if the function pointer is not NULL
+* and then calls the function pointed to with the given name
+* Return: void
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-    if (f == NULL)
-        return;
+	if (f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }

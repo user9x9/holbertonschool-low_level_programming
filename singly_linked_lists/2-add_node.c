cc97cbdc74780afd010393d2ae8dcc2cb851e01b
anil-akr <4nil000@protonmail.com>
2025-12-01 10:52:18 +0000

    Task 2 done

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* *add_node - Adds a new node at the beginning of a list_t list
+* @head: Double pointer to the head of the list
+* @str: String to duplicate and store in the new mode
+* Return: Address of the new mode, or NULL if it failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}

7cf0ae28508ae4eb280efd705d230b0679fdefbd
anil-akr <4nil000@protonmail.com>
2025-12-01 14:24:37 +0000

    Edit Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -13,19 +14,24 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+	 char *dup_str;
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	return (new_node);
+    dup_str = strdup(str);
+    if (dup_str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->str = dup_str;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+
+    *head = new_node;
+
+    return (new_node);
 }

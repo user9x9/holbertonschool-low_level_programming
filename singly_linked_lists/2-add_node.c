b0b58d3d6e52385b9a9c050ef1ee840712fd6e6e
anil-akr <4nil000@protonmail.com>
2025-12-02 10:05:31 +0000

    Fix tabs for betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,27 +14,27 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *dup_str;
-    unsigned int len = 0;
+	unsigned int len = 0;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = *head;
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = *head;
 
-    *head = new_node;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }

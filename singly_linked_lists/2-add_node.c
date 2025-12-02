29a6c0d6c0f4f3e8b56faa1d139fa0b031d0347a
anil-akr <4nil000@protonmail.com>
2025-12-02 10:01:42 +0000

    Remove strlen task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,8 +13,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-
-	 char *dup_str;
+	char *dup_str;
+    unsigned int len = 0;
 
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
@@ -27,8 +27,11 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     }
 
+    while (str[len] != '\0')
+        len++;
+
     new_node->str = dup_str;
-    new_node->len = strlen(str);
+    new_node->len = len;
     new_node->next = *head;
 
     *head = new_node;

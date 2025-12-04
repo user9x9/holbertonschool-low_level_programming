61b4d35a909f9ed3a79167c3eb9c0dba15d1a8ef
anil-akr <4nil000@protonmail.com>
2025-12-04 13:04:59 +0000

    Task 7 done

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Double pointer to the head of the doubly linked list
+ * @idx: The index where the new node should be added
+ * @n: The value to store in the new node
+ * Return: The address of the new node or NULL if it failed or if
+ * the index is not valid (out of range)
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+
+	if (idx == 0)
+	{
+		new = malloc(sizeof(dlistint_t));
+
+		if (new == NULL)
+			return (NULL);
+		
+		new->n = n;
+		new->prev = NULL;
+		new->next = *h;
+
+		if (*h != NULL)
+			(*h)->prev = new;
+		
+		*h = new;
+		return (new);
+	}
+	return (NULL);
+}

117e220cf774478eb5ad142dfae8b13b38124a56
anil-akr <4nil000@protonmail.com>
2025-12-05 09:30:07 +0000

    Task 7 fixed

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,33 +11,24 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
+	dlistint_t *tmp = *h, *new;
+	unsigned int i = 0;
+
+	if (h == NULL)
+		return (NULL);
 
 	if (idx == 0)
-	{
-		new = malloc(sizeof(dlistint_t));
+		return (add_dnodeint(h, n));
 
-		if (new == NULL)
-			return (NULL);
-		
-		new->n = n;
-		new->prev = NULL;
-		new->next = *h;
-
-		if (*h != NULL)
-			(*h)->prev = new;
-		
-		*h = new;
-		return (new);
-	}
-	tmp = *h;
-	while (tmp != NULL && i < index - 1)
+	while (tmp != NULL && i < idx - 1)
 	{
 		tmp = tmp->next;
 		i++;
 	}
 	if (tmp == NULL)
 		return (NULL);
+	if (tmp->next == NULL && i == idx - 1)
+		return (add_dnodeint_end(h, n));
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -46,9 +37,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = tmp->next;
 	new->prev = tmp;
 
-	if (tmp->next != NULL)
-		tmp->next->prev = new;
-
+	tmp->next->prev = new;
 	tmp->next = new;
+
 	return (new);
 }

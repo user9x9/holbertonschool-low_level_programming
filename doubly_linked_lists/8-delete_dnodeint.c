403f974d4e6fdf692e4a13b056bf4db67cb16f2b
anil-akr <4nil000@protonmail.com>
2025-12-05 09:09:33 +0000

    Task 8 test checker

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list
+ * @head: Pointer to pointer to head of the list
+ * @index: Index of the node to delete (starting at 0)
+ * Return: 1 if it succeeded -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	
+	tmp = *head;
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp != NULL && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (tmp == NULL)
+		return (-1);
+
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+
+	if (tmp->prev != NULL)
+		tmp->next->prev = tmp->prev;
+
+	free(tmp);
+	return (1);
+}

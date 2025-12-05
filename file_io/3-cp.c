59938cf1126b9724a3d9f5fb8e67589f2141b037
anil-akr <4nil000@protonmail.com>
2025-12-05 17:21:32 +0000

    Task 3 changes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,32 +3,35 @@
 #define BUF_SIZE 1024
 
 /**
- * open_from - open source file read-only
- * @file: source filename
- * Return: file descriptor or -1 on error
+ * open_from - opens a file for reading
+ * @file: name of the file to open
+ *
+ * Return: file descriptor on success, -1 on error
  */
 
 static int open_from(const char *file)
 {
-	int fd = open(file, O_RDONLY);
+	int fd;
 
+	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", file);
 	return (fd);
 }
 
-
 /**
- * open_to - open (or create) destination file truncated
- * @file: destination filename
- * Return: file descriptor or -1 on error
+ * open_to - opens/creates a file for writing (truncate if exists)
+ * @file: name of the file to open/create
+ *
+ * Return: file descriptor on success, -1 on error
  */
 
 static int open_to(const char *file)
 {
-	int fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int fd;
 
+	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd < 0)
 		dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", file);
@@ -36,10 +39,10 @@ static int open_to(const char *file)
 }
 
 /**
- * copy_fd - copy content from fd_from to fd_to
+ * copy_fd - copy the content from one file descriptor to another
  * @fd_from: source file descriptor
  * @fd_to: destination file descriptor
- * Return: 0 on success, -1 on error
+ * Return: 0 on success, -1 on failure
  */
 
 static int copy_fd(int fd_from, int fd_to)
@@ -53,19 +56,21 @@ static int copy_fd(int fd_from, int fd_to)
 		if (w != r)
 			return (-1);
 	}
-	return (r < 0 ? -1 : 0);
+	if (r < 0)
+		return (-1);
+	return (0);
 }
 
 /**
- * main - copy a file to another
- * @ac: arg count
- * @av: arg vector
- * Return: 0 on success, exits with codes on error
+ * main - copies the content of a file to another file
+ * @ac: number of arguments
+ * @av: argument vector
+ * Return: 0 on success, otherwise exit with error code
  */
 
 int main(int ac, char **av)
 {
-	int f1, f2, rc;
+	int f1, f2;
 
 	if (ac != 3)
 	{
